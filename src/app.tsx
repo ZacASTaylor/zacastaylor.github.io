592a040427bcad96304cd5d6cc706d731819cdbe
ZacASTaylor <zacharystroudtaylor@cmail.carleton.ca>
2020-05-17 20:36:24 -0400

    Initial config

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import {hot} from 'react-hot-loader/root';
+import React, {useState} from 'react';
+
+
+function App(props) {
+    return <h1 style={{color:"#0099bf"}} >Welcome to my website! </h1>
+}
+
+export default hot(App);
